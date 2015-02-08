1d3bda1fc63c35490802b3806ed7e062cfc1dbe4
Mojo Major <info@redroostermobile.com>
2015-02-08 23:04:05 +0100

    still some weird behaviour but generally working

--- app/controllers/ws_polls_controller.rb
+++ app/controllers/ws_polls_controller.rb
@@ -7,16 +7,16 @@ class WsPollsController < WebsocketRails::BaseController
     # or check https://github.com/lukas2/websockets_chat
   end
 
-  before_action :set_poll, only: [:revoke_vote,:add_candidate,:vote_for_candidate]
+  before_action :set_poll, only: [:revoke_vote, :add_candidate, :vote_for_candidate]
   before_action :set_vote_by_nickname, only: [:vote_for_candidate]
   #before_action :set_candidate_for_voting, only: [:vote_for_candidate]
 
   def add_candidate
     # prevent multiple adds of the same candidate-name for a poll
     if @poll.candidates.where(name: message[:name]).length == 0
-      candidates = @poll.candidates.push(Candidate.new(name:message[:name]))
+      candidates = @poll.candidates.push(Candidate.new(name: message[:name]))
 
-      trigger_success( message: true)
+      trigger_success(message: true)
 
       last_candidate = candidates.last
       votes = last_candidate.votes
@@ -31,9 +31,9 @@ class WsPollsController < WebsocketRails::BaseController
       return_hash['votes'] = vote_array
 
       # todo: add some hashvalue to avoid duplicated urls
-      WebsocketRails[@poll.url.to_sym].trigger(:new_candidate, return_hash )
+      WebsocketRails[@poll.url.to_sym].trigger(:new_candidate, return_hash)
     else
-      trigger_failure ( {message: "option with name #{message[:name]} exists already for this poll"})
+      trigger_failure ({message: "option with name #{message[:name]} exists already for this poll"})
     end
   end
 
@@ -42,21 +42,20 @@ class WsPollsController < WebsocketRails::BaseController
     vote_id = message[:vote_id]
     candidate_id = message[:candidate_id]
 
-    vote = Vote.find(vote_id)
-    if vote.nickname==nickname
-      if vote
-        vote_attribs=vote.attributes
-        trigger_success ( {message: 'vote revoked'})
-        vote.delete
-        # trigger update of all channel members
-        WebsocketRails[@poll.url.to_sym].trigger(:revoked_vote, {candidate_id: candidate_id, vote: vote_attribs} )
-      else
-        trigger_failure ( {message: 'vote not found'})
-      end
+    vote = Vote.where(id: vote_id)
+    if vote.first
+        if vote.first.nickname==nickname
+          vote_attribs=vote.first.attributes
+          trigger_success ({message: 'vote revoked', attr: vote_attribs})
+          vote.first.delete
+          # trigger update of all channel members
+          WebsocketRails[@poll.url.to_sym].trigger(:revoked_vote, {candidate_id: candidate_id, vote: vote_attribs})
+        else
+          trigger_failure ({message: 'this was not your vote'})
+        end
     else
-      trigger_failure ( {message: 'this was not your vote'})
+      trigger_failure ({message: 'vote not found'})
     end
-
   end
 
   def vote_for_candidate
@@ -64,18 +63,18 @@ class WsPollsController < WebsocketRails::BaseController
 
     if @vote
       # already voted for this poll
-      trigger_failure ( {message: 'you already voted for this poll'})
+      trigger_failure ({message: 'you already voted for this poll'})
     else
       # create vote on poll
       # trigger success
       puts message
       candidate_id = message[:candidate_id]
-      @new_vote = Vote.new(poll_id: @poll.id, candidate_id: candidate_id,nickname: message[:nickname])
+      @new_vote = Vote.new(poll_id: @poll.id, candidate_id: candidate_id, nickname: message[:nickname])
 
-      trigger_success( message: @new_vote.save)
+      trigger_success(message: @new_vote.save)
 
       # trigger update of all channel members
-      WebsocketRails[@poll.url.to_sym].trigger(:new_vote, {candidate_id: candidate_id, vote: @new_vote} )
+      WebsocketRails[@poll.url.to_sym].trigger(:new_vote, {candidate_id: candidate_id, vote: @new_vote})
 
       # todo: update votes on clients
     end
@@ -90,7 +89,7 @@ class WsPollsController < WebsocketRails::BaseController
     candidate = Candidate.find message[:candidate_id]
     puts 'set_candidate_for_voting'
     if @poll.id == candidate.poll_id
-       # this is actually not supposed to happen
+      # this is actually not supposed to happen
       if candidate.votes.where(nickname: message[:nickname]).length > 0
         # voted already
       else
